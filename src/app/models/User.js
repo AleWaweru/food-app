188830e678c3e9fb2a8e66c237d31dae4d7f5dbb
AleWaweru <ngashalex8@gmail.com>
2023-11-30 18:59:11 +0300

    configured Registration

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema({
@@ -12,6 +13,11 @@ const UserSchema = new Schema({
     },
 }, { timestamps: true });
 
+UserSchema.post('validate', function (user) {
+    const notHashedPassword = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(notHashedPassword, salt);
+})
 const User = models?.User || model('User', UserSchema);
 
 export default User;
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -6,25 +6,30 @@ import Image from "next/image";
 const RegisterPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
 
-  function handleFormSubmit (e) {
+  async function handleFormSubmit (e) {
     e.preventDefault();
-    fetch('/api/register', {
+    setCreatingUser(true)
+    await fetch('/api/register', {
       method: 'POST',
       body: JSON.stringify({email, password}),
       headers: {'Content-type': 'application/json'},
     })
+
+    setCreatingUser(false);
 
   }
   return (
     <section>
         <h1 className='text-center text-primary text-4xl'>Register</h1>
         <form className='block max-w-xs mx-auto' onSubmit={handleFormSubmit}>
-            <input type='email' placeholder='email' value={email} 
+            <input  disabled={creatingUser}  type='email' placeholder='email' value={email} 
             onChange={e =>setEmail( e.target.value)}></input>
-            <input type='password' placeholder='password' value={password}
+            <input disabled={creatingUser} type='password' placeholder='password' value={password}
             onChange={e => setPassword(e.target.value)}></input>
-            <button type='submit'>Register</button>
+            <button  disabled={creatingUser}  type='submit'>Register</button>
             <div className='my-4 text-center text-gray-500'>
                 or login with provider
             </div>
